23bc1c369a4e7f3d489cf29169d848f3e41daabd
Maljean <maljean@student.42.us.org>
2019-07-02 17:16:37 -0700

    Initial Capsule

---
+++ fantasyPicks/code/lib/mock.js
@@ -0,0 +1,104 @@
+
+module.exports =[
+  {
+	name: "Tom Brady",
+	id: "Tom_Brady",
+  expertsPick: true,
+	info: {
+		team: "QB - NE",
+		vs: "vs. IND, Thu 8pm EDT"
+	},
+	image: {
+		images: [{
+			url: 'https://images.fantasypros.com/images/nfl/players/210x210/9603.jpg'
+		}]
+	},
+	pick: {
+		percentage: "99%",
+		note: "Recommended by 114 of 115 experts"
+	},
+	fantasyPoints: {
+		seasonTotal: 64.2,
+		avgPerGame: 16.1,
+		lastWeek: 19.0,
+	},
+	misc: {
+		injury: "None"
+	}
+},
+  {
+	name: "Ezekiel Elliott",
+	id: "Ezekiel_Elliott",
+  expertsPick: false,
+	info: {
+		team: "RB - DAL",
+		vs: "at HOU, Sun 8pm EDT"
+	},
+	image: {
+		images: [{
+			url: 'https://images.fantasypros.com/images/nfl/players/210x210/15498.jpg'
+		}]
+	},
+	pick: {
+		percentage: "1%",
+		note: "Recommended by 1 of 115 experts"
+	},
+	fantasyPoints: {
+		seasonTotal: 71.1,
+		avgPerGame: 17.8,
+		lastWeek: 30.0,
+	},
+	misc: {
+		injury: "None"
+	}
+},{
+	name: "Dion Lewis",
+	id: "Dion_Lewis",
+  expertsPick: true,
+	info: {
+		team: "RB - TEN",
+		vs: "at BUF, Sun 1pm EDT"
+	},
+	image: {
+		images: [{
+			url: 'https://images.fantasypros.com/images/nfl/players/210x210/9983.jpg'
+		}]
+	},
+	pick: {
+		percentage: "51%",
+		note: "Recommended by 60 of 118 experts"
+	},
+	fantasyPoints: {
+		seasonTotal: 31.9,
+		avgPerGame: 8.0,
+		lastWeek: 6.6,
+	},
+	misc: {
+		injury: "None"
+	}
+}, {
+	name: "Tyler Boyd",
+	id: "Tyler_Boyd",
+  expertsPick: false,
+	info: {
+		team: "WR - CIN",
+		vs: "vs. MIA, Sun 1pm EDT"
+	},
+	image: {
+		images: [{
+			url: 'https://images.fantasypros.com/images/nfl/players/210x210/15547.jpg'
+		}]
+	},
+	pick: {
+		percentage: "49%",
+		note: "Recommended by 58 of 118 experts"
+	},
+	fantasyPoints: {
+		seasonTotal: 47.4,
+		avgPerGame: 11.9,
+		lastWeek: 10.0,
+	},
+	misc: {
+		injury: "None"
+	}
+}];
