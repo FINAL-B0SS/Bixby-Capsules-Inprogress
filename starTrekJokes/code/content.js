d1b1fa0362c062530779c743cca8708738d80d78
root <root@Tiny-Satan.localdomain>
2019-07-13 18:10:54 -0700

    new capsule

---
+++ starTrekJokes/code/content.js
@@ -0,0 +1,42 @@
+module.exports = [
+  // Replace content below to customize
+  // tags are optional and used for "find facts about [tag]" etc
+  // image is optional - displayed on UI
+  // START PASTE FROM SPREADSHEET HERE
+    {
+    question: "Why was Star Trek so successful?",
+    answer: "It had good Genes.",
+    image: {
+      url: "images/Gene.jpg"
+    }
+  },
+  {
+    question: "What does a Romulan frog use for camouflage?",
+    answer: "A croaking device",
+    image: {
+      url: "images/Romulan.jpg"
+    }
+  },
+  {
+    question: "Why don't the Borg go to prison?",
+    answer: "Because they obey the Lore!",
+    image: {
+      url: "images/Borg_drone.jpg"
+    }
+  },
+   {
+    question: "What do you call it when that Strategic Operations Officer on DS9 runs as fast as he can?",
+    answer: "Worf Speed.",
+    image: {
+      url: "images/Worf.jpg"
+    }
+  },
+    {
+    question: "Did you hear that the crew of the Enterprise is getting married? They have engaged the Borg",
+    answer: "It had good Genes.",
+    image: {
+    url: "images/Borg.jpg"
+    }
+    },
+  // END PASTE FROM SPREADSHEET HERE
+]
