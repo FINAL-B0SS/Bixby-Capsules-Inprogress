ac2269ee740cba45ab970a5654c30ad13881ea2b
Maljean <maljean@student.42.us.org>
2019-07-13 20:11:03 -0700

    Added more jokes + tags

--- starTrekJokes/code/content.js
+++ starTrekJokes/code/content.js
@@ -5,6 +5,7 @@ module.exports = [
   // START PASTE FROM SPREADSHEET HERE
     {
     question: "Why was Star Trek so successful?",
+    tags: ["star trek", "star", "trek"],
     answer: "It had good Genes.",
     image: {
       url: "images/Gene.jpg"
@@ -13,6 +14,7 @@ module.exports = [
   {
     question: "What does a Romulan frog use for camouflage?",
     answer: "A croaking device",
+    tags: ["star trek", "star", "trek"],
     image: {
       url: "images/Romulan.jpg"
     }
@@ -20,6 +22,7 @@ module.exports = [
   {
     question: "Why don't the Borg go to prison?",
     answer: "Because they obey the Lore!",
+    tags: ["star trek", "star", "trek"],
     image: {
       url: "images/Borg_drone.jpg"
     }
@@ -27,16 +30,238 @@ module.exports = [
    {
     question: "What do you call it when that Strategic Operations Officer on DS9 runs as fast as he can?",
     answer: "Worf Speed.",
+     tags: ["star trek", "star", "trek"],
     image: {
       url: "images/Worf.jpg"
     }
   },
-    {
+  {
     question: "Did you hear that the crew of the Enterprise is getting married? They have engaged the Borg",
     answer: "It had good Genes.",
+    tags: ["star trek", "star", "trek"],
     image: {
     url: "images/Borg.jpg"
     }
-    },
+  },
+  {
+    question: "What does the Enterprise and Toliet paper have in common?",
+    answer: "They both circle Uranus wiping out Klingons.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "What does a Romulan frog use for camouflage?",
+    answer: " A croaking device.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Why don't the Borg go to prison?",
+    answer: "Because they obey the Lore!",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Why did the Borg cross the road?",
+    answer: "Because it assimilated the chicken!",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "What did Spock find in Kirk's toliet?",
+    answer: "The Captian's Log",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Have you heard the new Klingon army motto?",
+    answer: "Join the Klingon army. Visit exotic planets, meet interesting people, and kill them!",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Where do the Borg eat fast food?",
+    answer: "At their local Borger King!",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Why was Star Trek so successful?",
+    answer: "It had good Genes.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "How many ears does Picard have?",
+    answer: "Three. A right ear. A left ear. And a final front ear.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "What is Captain Picards biggest pet peeve?",
+    answer: "When the crew replaces his dilithium crystals with Folgers crystals.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Did you hear that the crew of the Enterprise is getting married?",
+    answer: "They have engaged the Borg.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "How many Romulans does it take to screw in a light bulb?",
+    answer: "ONE HUNDRED FIFTY_ONE: One to screw the light bulb in, and 150 to self-destruct the ship out of disgrace.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "How many Vulcans does it take to screw in a light bulb? ",
+    answer: "Approximately 1.00000000000000000000000000000000",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "How many Borg does it take to change a light-bulb?",
+    answer: "All of them! ",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "How many Klingons does it take to change a lightbulb?",
+    answer: "TWO: One to screw it in, and one to stab the other in the back and take all of the credit.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "How many Klingons does it take to change a lightbulb?",
+    answer: "NONE: Klingons aren't afraid of the dark.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "What do the Klingons do with the dead bulb?",
+    answer: "Execute it for failure.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "What is Commander Rikers favorite hobby?",
+    answer: "Sewing, because the captain says",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "What do the Klingons do with the Klingon who replaces the bulb?",
+    answer: "Execute him for cowardice.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Have you read the book The Positronic Brain?",
+    answer: " It's by: Anne Droid",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Why Did Lieutenant Uhuru look so shocked?",
+    answer: "Because William Shatner (shat in her).",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Have you read the book Damn it Jim?",
+    answer: "It's by: Ima Doctor and Nada Bricklayer.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Have you read the book Chekov: The Navigator?",
+    answer: "It's by: I. Kiptin",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "What did one Borg say to one another right before their ship was destroyed in sector zero zero one?",
+    answer: "Hoisted by our own Picard. ",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Did you hear about the new uniform making machine on the Enterprise?",
+    answer: "Piccard told Riker to Make it sew, Number One.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "How do you get a one-armed Klingon out of a tree?",
+    answer: "Wave to him",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "What did the first officer answer when Picard asked Why did you let Troi win at poker?",
+    answer: "Because I Riker.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "What is Thomas Riker's dating philosophy?",
+    answer: "If at first you don't succeed, try Troi again.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Why can't Klingon kids play in sandboxes?",
+    answer: "Cats keep trying to cover them up.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "What did Worf say when small ice asteroids began hitting the Enterprise hull?",
+    answer: "Captain, we are being hailed.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Did you hear about the Federation weapons expert?",
+    answer: "He never forgets a phaser.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "What are eyeglasses called on Vulcan?",
+    answer: "Spocktacles Cross The Road",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Why did the Klingon cross the road?",
+    answer: "To conquer the other side.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Why did the chicken cross the road?",
+    answer: "James T. Kirk: To boldly go where no chicken has gone before",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Why did the chicken cross the road?",
+    answer: "Cos ma wee transporter beam was na functioning properly. Ah canna work miracles, Captain.",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Why did the chicken cross the road?",
+    answer: "Dr. McCoy: Dammit Jim!! I'm a doctor not an farmer!",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Obviously, it was the logical thing to do.",
+    answer: "Why did the chicken cross the road?",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Why did the chicken cross the road?",
+    answer: "Mr. Data: Why is a barn yard fowl crossing a thoroughfare humorous?",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "Sarek and Amanda were dating. \n\Amanda was patiently waiting. \n\For signs of romance. \n\Soft words, a slow dance. \n\What she got was an efficiency rating", 
+    answer: " ",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "A visiting admiral approached Chekov's station on the ENTERPRISE. Thinking he would test the young officer, he asked, What would you do if the weapons officer suddenly got his head blown off? \n\Nothing, sir. \n\Why nothing? \n\Because I'm the weapons officer, sir.",
+    answer: " ",
+    tags: ["star trek", "star", "trek"],
+  },
+  {
+    question: "A young man was applying to join Starfleet \n\Where were you born? asked the recruiting officer. \n\Earth, sir. \n\What part? \n\All of me, sir.", 
+    answer: " ",
+    tags: ["star trek", "star", "trek", "bar"]
+  },
+  {
+    question: "Overheard in a corridor: \n\Crewman: I've got a brother at Starfleet Science Academy. \n\Crewwoman: What's he studying? \n\Crewman: Nothin'. They're studying him.",
+    answer: " ",
+    tags: ["star trek", "star", "trek", "bar"]
+  },
+  {
+    question: "Why did the chicken cross the road?",
+    answer: "Computer: Insufficient information",
+    tags: ["star trek", "star", "trek", "bar"]
+  },
+  {
+    question: "Why did the chicken cross the road?",
+    answer: "Computer: Insufficient information",
+    tags: ["star trek", "star", "trek", "bar"]
+  },
   // END PASTE FROM SPREADSHEET HERE
 ]
