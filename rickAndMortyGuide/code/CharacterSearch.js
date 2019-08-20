cf57d6e54d052b54235e2f49dcd8536b001e6a2a
Maljean <maljean@student.42.us.org>
2019-08-19 22:00:18 -0700

    working on api parsing

--- rickAndMortyGuide/code/CharacterSearch.js
+++ rickAndMortyGuide/code/CharacterSearch.js
@@ -2,7 +2,6 @@ var http = require('http')
 var console = require('console')
 
 function build_episode(id) {
-
 var episodes = JSON.parse(http.getUrl("https://rickandmortyapi.com/api/episode/" + id, {format: 'text'}))
 var ret = {
   name: episodes.name,
@@ -11,7 +10,7 @@ var ret = {
   characters: []
 }
 
-for (var i = 1; i < episodes.characters.length; i += 1) {
+for (var i = 0; i < episodes.characters.length; i += 1) {
   var characters = JSON.parse(http.getUrl("https://rickandmortyapi.com/api/character/" + Number(episodes.characters[i].match(/\d+/), {format: 'text'})))
   ret.characters.push({ 
     name: characters.name,
@@ -23,6 +22,7 @@ for (var i = 1; i < episodes.characters.length; i += 1) {
 
 function build_character(id) {
   var characters = JSON.parse(http.getUrl("https://rickandmortyapi.com/api/character/" + id, {format: 'text'}))
+
   var ret = {
       name: characters.name,
       status: characters.status,
@@ -36,9 +36,9 @@ function build_character(id) {
   if (characters.type) {
     ret.type = characters.type
   }
-   for (var i = 1; i < characters.episode.length; i += 1) {
-     ret.episodes.push((build_episode(Number(characters.episode[i].match(/\d+/)))))
-   }
+  for (var i = 0; i < characters.episode.length; i += 1) {
+    ret.episodes.push((build_episode(Number(characters.episode[i].match(/\d+/)))))
+  }
   return ret
 }
 
@@ -67,11 +67,12 @@ exports.function = function (text) {
   var characters = JSON.parse(http.getUrl("https://rickandmortyapi.com/api/character/1", {format: 'text'}))
   var ret = []
 
-  for (var i = 1; i < 130; i += 1) {
+  for (var i = 1; i < 3/*494*/; i += 1) {
     var person = build_character(i)
     if (person.name == text || text == 'all' || !text) {
       ret.push(person)
     }
+    
   }
   return ret
 }
