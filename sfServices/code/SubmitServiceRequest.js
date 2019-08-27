709cdbe6a7b87194ebb4af5eb06444b6c1f22b24
Maljean <maljean@student.42.us.org>
2019-08-26 19:30:40 -0700

    Building optional params

--- sfServices/code/SubmitServiceRequest.js
+++ sfServices/code/SubmitServiceRequest.js
@@ -2,22 +2,14 @@ var http = require('http')
 var console = require('console')
 //[{"token":"5d57c95be8969e867076606d"}]
 // 5d632bb9195448dfebb788e4
-module.exports = function SubmitServiceRequest (description, makeModel, licensePlate, color, firstName, lastName, email, phone, objectID, object, natureOfRequest, howManyPeople, howManyTents, poleType, type, containsRacialSlursOrProfanity, wholeBlock, location, serviceInfo) {
+module.exports = function SubmitServiceRequest (description, makeModel, licensePlate, firstName, lastName, email, phone, objectID, object, natureOfRequest, howManyPeople, howManyTents, poleType, type, containsRacialSlursOrProfanity, wholeBlock, location, serviceInfo) {
   var url = "http://mobile311-dev.sfgov.org/open311/v2/requests.json"
   var data = http.getUrl(url, {format: 'text'})
   var ret = JSON.parse(data)
   var options = {
     format: "json",
     query: {
-      api_key: "a4cb845ebcb8c15b4fb2f79fdd28f0e6",
-      // service_code: serviceInfo.serviceCode,
-      // lat: location.point.latitude,
-      // long: location.point.longitude,
-      // first_name: firstName,
-      // last_name: lastName,
-      // email: email,
-      // phone: phone,
-      // description: description,
+      api_key: "a4cb845ebcb8c15b4fb2f79fdd28f0e6"
     }
   }
   var request = {
@@ -36,8 +28,6 @@ module.exports = function SubmitServiceRequest (description, makeModel, licenseP
     request.make_model = makeModel
   if (licensePlate)
    request.license_plate = licensePlate
-  if (color)
-   request.color = color
   if (objectID)
    request.object_id = objectID
   if (object)
@@ -59,6 +49,6 @@ module.exports = function SubmitServiceRequest (description, makeModel, licenseP
   if (serviceInfo.serviceName)
    request.service_name = serviceInfo.serviceName
 
-  http.postUrl(url, request, options)
+  //http.postUrl(url, request, options)
   return "2"
 }
