254f42c3831ccc24f10aa30f5a1cdf948910febc
Maljean <maljean@student.42.us.org>
2019-09-03 17:41:41 -0700

    new folder name

---
+++ sportsFeeds/code/fetchSportsList.js
@@ -0,0 +1,19 @@
+module.exports.function = function fetchSportsList () {
+  // poker and soccer are broken
+  return ["Top ⭐",
+  "NFL 🏈",
+   "NBA 🏀",
+   "MLB ⚾",
+   "NHL 🏒",
+   "Golf 🏌️‍♀️",
+   "Soccer ⚽",
+   "Autos 🏎",
+   "Tennis 🎾",
+   "Boxing 🥊",
+   "College Basketball 🏀",
+   "College Football 🏈",
+   "College Sports 🎓",
+   "Olympic Sports 🥇",
+   "Horse Racing 🏇",
+   "Poker 🃏"]
+}
