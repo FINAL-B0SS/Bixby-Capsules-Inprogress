6d6c009592522744bd77767c0fad94dd07c7691b
Maljean <maljean@student.42.us.org>
2019-09-10 21:00:54 -0700

    Created template for rss feeds

---
+++ RSSFeeds/code/feeds.js
@@ -0,0 +1,56 @@
+module.exports = [{
+      url: 'https://www.wired.com/feed/category/business/latest/rss',
+      text: 'Business',
+      tags: ['business']
+    },
+    {
+      url: 'https://www.wired.com/feed/category/culture/latest/rss',
+      text: 'Culture',
+      tags: ['culture', 'cultural']
+    },
+    {
+      url: 'https://www.wired.com/feed/rss',
+      text: 'Top',
+      tags: ['top']
+    },
+    {
+      url: 'https://www.wired.com/feed/category/gear/latest/rss',
+      text: 'Gear',
+      tags: ['gear', 'products', 'reviews', 'essentials']
+    },
+    {
+      url: 'https://www.wired.com/feed/category/ideas/latest/rss',
+      text: 'Ideas',
+      tags: ['ideas', 'provacative', 'enlightening', 'theories', 'ruminations', 'thought provoking']
+    },
+    {
+      url: 'https://www.wired.com/feed/category/science/latest/rss',
+      text: 'Science',
+      tags: ['science', 'research', 'studies']
+    },
+    {
+      url: 'https://www.wired.com/feed/category/security/latest/rss',
+      text: 'Security',
+      tags: ['security']
+    },
+    {
+      url: 'https://www.wired.com/feed/category/transportation/latest/rss',
+      text: 'Transportation',
+      tags: ['transportation']
+    },
+    {
+      url: 'https://www.wired.com/feed/category/backchannel/latest/rss',
+      text: 'Back Channel',
+      tags: ['backchannel']
+    },
+    {
+      url: 'https://www.wired.com/feed/tag/wired-guide/latest/rss',
+      text: 'Wired Guide',
+      tags: ['wired guide', 'guide']
+    },
+    {
+      url: 'https://www.wired.com/feed/category/photo/latest/rss',
+      text: 'Photo',
+      tags: ['photo', 'imagery']
+    }
+  ]
