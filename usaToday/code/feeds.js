c4173aed47667380c7975ec93901081ea83260fb
Maljean <maljean@student.42.us.org>
2019-10-02 01:56:04 -0700

    USA Today - Created initial feeds

---
+++ usaToday/code/feeds.js
@@ -0,0 +1,229 @@
+// Add RSS feeds below
+// Url: Link to RSS feed xml
+// Text: What users see in feed selection list
+// Tags: What the user can say to automatically call certain feeds
+module.exports = module.exports = [
+  {
+    url: 'http://rssfeeds.usatoday.com/usatoday-NewsTopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'All Top Headlines',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomNation-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'National',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomWashington-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Washington',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomWorld-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'World',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/News-Opinion',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Opinion',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomSports-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'All Sports Headlines',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomNfl-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'NFL',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomCollegeFootball-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'NCAAF',
+    tags: ['']
+  },
+  // DEAD LINK
+  // {
+  //   url: 'http://rssfeeds.usatoday.com/UsatodaycomMlb-TopStories',
+  //   urlText: 'Click here to see the full story on usatoday.com',
+  //   text: 'MLB',
+  //   tags: ['']
+  // },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomNba-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'NBA',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomGolf-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Golf',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/topfantasy',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Fantasy',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomNhl-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'NHL',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomCollegeMensBasketball-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'NCAAB',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomSoccer-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Soccer',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/topmotorsports',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Motor Sports',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodayTennis-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Tennis',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodayCycling-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Cycling',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomOlympicsCoverage-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Olympics',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/mmajunkie',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'MMA Junkie',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/usatoday-LifeTopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'All Entertainment Headlines',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/toppeople',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'People',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/allthemoms',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'All The Moms',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomMovies-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Movies',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomMusic-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Music',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomTelevision-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'TV',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomBooks-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Books',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomMoney-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'All Money Headlines',
+    tags: ['']
+  },
+  {
+    url: 'Cars',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'http://rssfeeds.usatoday.com/UsatodaycomMoney-Healey',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/usatoday-TechTopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'All Tech Headlines',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomTech-PersonalTalk',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Personal Tech',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/topgaming',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Gaming',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodaycomTravel-TopStories',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'All Travel Headlines',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodayTravel-Destinations',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Destinations',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodayTravel-Flights',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Flights',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/TP-TheCruiseLog',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Cruises',
+    tags: ['']
+  },
+  {
+    url: 'http://rssfeeds.usatoday.com/UsatodayTravel-Hotels',
+    urlText: 'Click here to see the full story on usatoday.com',
+    text: 'Hotels',
+    tags: ['']
+  },
+]
