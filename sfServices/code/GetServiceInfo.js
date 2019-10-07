358c1586dce971b33fb57d04cc624b6cc2ea15df
Maljean <maljean@student.42.us.org>
2019-10-07 07:05:08 -0700

    SF Services - Service info array is now built using map

--- sfServices/code/GetServiceInfo.js
+++ sfServices/code/GetServiceInfo.js
@@ -1,20 +1,16 @@
-var console = require('console')
 var http = require('http')
 var secret = require('secret')
 
 module.exports.function = function GetServiceInfo() {
   var url = secret.get('url.services')
   var ret = http.getUrl(url, { format: 'json' })
-  var services = []
 
-  for (var i = 0; i < ret.length; i++) {
-    var template = {
-      serviceName: ret[i].service_name,
-      group: ret[i].group,
-      description: String(ret[i].description).replace(/<[^>]*>?/gm, ''),
-      serviceCode: ret[i].service_code
+  return ret.map(function(info) {
+    return {
+      serviceName: info.service_name,
+      group: info.group,
+      description: String(info.description).replace(/<[^>]*>?/gm, ''),
+      serviceCode: info.service_code
     }
-    services.push(template)
-  }
-  return services
+  })
 }
