1270fea62632c54f9e8c69f9b29329e1f84197c7
Maljean <maljean@student.42.us.org>
2019-10-07 06:16:55 -0700

    SF Service - Updated all views with better HEF, and dialog

--- sfServices/code/GetServiceInfo.js
+++ sfServices/code/GetServiceInfo.js
@@ -2,23 +2,19 @@ var console = require('console')
 var http = require('http')
 var secret = require('secret')
 
-module.exports.function = function GetServiceInfo(voiceName) {
+module.exports.function = function GetServiceInfo() {
   var url = secret.get('url.services')
   var ret = http.getUrl(url, { format: 'json' })
   var services = []
-  var voiceServices = []
-  var template
 
   for (var i = 0; i < ret.length; i++) {
-    template = {
+    var template = {
       serviceName: ret[i].service_name,
       group: ret[i].group,
       description: String(ret[i].description).replace(/<[^>]*>?/gm, ''),
       serviceCode: ret[i].service_code
     }
-    if (voiceName && ret[i].service_name.toLowerCase().replace('&', 'and').includes(voiceName.toLowerCase()))
-      voiceServices.push(template)
     services.push(template)
   }
-  return voiceServices.length > 0 ? voiceServices : services
+  return services
 }
