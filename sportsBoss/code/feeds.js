cd39356b629bbeca7720ed02df6117afd7a4d961
Maljean <maljean@student.42.us.org>
2019-10-26 04:34:53 -0700

    Sports Boss - Updated category selection view and fixed depreciations

--- sportsBoss/code/feeds.js
+++ sportsBoss/code/feeds.js
@@ -1,102 +1,120 @@
 // Add RSS feeds below
-// Url: Link to RSS feed xml
+// Url: Link to RSS feed xml (url must start with 'https://')
 // Text: What users see in feed selection list
+// Description: A description of what this is link contains.
 // Tags: What the user can say to automatically call certain feeds
+// Copyright: Feeds copyright message to use if the feed doesn't have one
 module.exports = module.exports = [
   {
     url: 'https://www.espn.com/espn/rss/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'Top',
+    image: 'https://a.espncdn.com/i/espn/teamlogos/lrg/trans/espn_dotcom_black.gif',
     tags: ['top', 'trending', 'popular']
   },
   {
     url: 'https://www.espn.com/espn/rss/nfl/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'NFL',
+    image: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/leagues/500/nfl.png?w=100&h=100&transparent=true',
     tags: ['nfl', 'football', 'national football league']
   },
   {
     url: 'https://www.espn.com/espn/rss/nba/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'NBA',
+    image: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/leagues/500/nba.png?w=100&h=100&transparent=true',
     tags: ['nba', 'basketball', 'national basketball association']
   },
   {
     url: 'https://www.espn.com/espn/rss/mlb/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'MLB',
+    image: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/leagues/500/mlb.png?w=100&h=100&transparent=true',
     tags: ['baseball', 'major league baseball', 'mlb']
   },
   {
     url: 'https://www.espn.com/espn/rss/nhl/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'NHL',
+    image: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/leagues/500/nhl.png?w=100&h=100&transparent=true',
     tags: ['hockey', 'national hockey league', 'nhl']
   },
   {
     url: 'https://www.espn.com/espn/rss/golf/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'Golf',
+    image: 'https://a.espncdn.com/combiner/i?img=/redesign/assets/img/icons/ESPN-icon-golf.png&h=160&w=160&scale=crop&cquality=40',
     tags: ['golf', 'golfer', 'golfing']
   },
   {
     url: 'https://www.espn.com/espn/rss/poker/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'Poker',
+    image: 'https://a.espncdn.com/i/espn/networks_shows/500/espn_chalk.png',
     tags: ['poker']
   },
   {
     url: 'https://www.espn.com/espn/rss/rpm/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'RPM',
+    image: 'https://a.espncdn.com/combiner/i?img=/redesign/assets/img/icons/ESPN-icon-nascar.png&h=160&w=160&scale=crop&cquality=40',
     tags: ['auto', 'car', 'race', 'racing', 'rpm']
   },
   {
     url: 'https://www.espn.com/espn/rss/tennis/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'Tennis',
+    image: 'https://a.espncdn.com/combiner/i?img=/redesign/assets/img/icons/ESPN-icon-tennis.png&h=160&w=160&scale=crop&cquality=40',
     tags: ['tennis']
   },
   {
     url: 'https://www.espn.com/espn/rss/boxing/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'Boxing',
+    image: 'https://a.espncdn.com/combiner/i?img=/redesign/assets/img/icons/ESPN-icon-boxing.png&h=160&w=160&scale=crop&cquality=40',
     tags: ['boxing', 'boxer']
   },
   {
     url: 'https://www.espn.com/espn/rss/soccer/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'Soccer',
+    image: 'https://a.espncdn.com/combiner/i?img=/redesign/assets/img/icons/ESPN-icon-soccer.png&h=160&w=160&scale=crop&cquality=40',
     tags: ['soccer']
   },
   {
     url: 'https://www.espn.com/espn/rss/ncb/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'College Basketball',
+    image: 'https://a.espncdn.com/combiner/i?img=/redesign/assets/img/icons/ESPN-icon-basketball.png&h=160&w=160&scale=crop&cquality=40',
     tags: ['college basketball', 'ncb', 'national college basketball']
   },
   {
     url: 'https://www.espn.com/espn/rss/ncf/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'College Football',
+    image: 'https://a.espncdn.com/combiner/i?img=/redesign/assets/img/icons/ESPN-icon-football-college.png&h=160&w=160&scale=crop&cquality=40',
     tags: ['college football', 'ncf', 'national college football']
   },
   {
     url: 'https://www.espn.com/espn/rss/ncaa/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'NCAA',
+    image: 'https://a3.espncdn.com/combiner/i?img=%2Fredesign%2Fassets%2Fimg%2Ficons%2FESPN%2Dicon%2Dall%2Dsports%2Dcollege.png&w=80&h=80&scale=crop&cquality=40&location=origin',
     tags: ['national collegiate athletic association', 'ncaa', 'national college athletic association']
   },
   {
     url: 'https://www.espn.com/espn/rss/oly/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'Olympic',
+    image: 'https://a.espncdn.com/combiner/i?img=/redesign/assets/img/icons/ESPN-icon-olympics.png&h=160&w=160&scale=crop&cquality=40',
     tags: ['olympic', 'olympian']
   },
   {
     url: 'https://www.espn.com/espn/rss/horse/news',
     urlText: 'Click here to see the full story on espn.com',
     text: 'Horse Racing',
+    image: 'https://a.espncdn.com/combiner/i?img=/redesign/assets/img/icons/ESPN-icon-equestrian.png&h=160&w=160&scale=crop&cquality=40',
     tags: ['horse', 'horse races', 'horse racing', 'horse race']
   }
 ]
